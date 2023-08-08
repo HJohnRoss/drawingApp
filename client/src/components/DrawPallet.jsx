d75acd36ebbb6bbb046a4d3b5fc55dd79a36441b
FJohnHRoss <rossjohn936@gmail.com>
2023-08-07 17:38:10 -0700

    pallet refactoring

--- client/src/components/DrawPallet.jsx
+++ client/src/components/DrawPallet.jsx
@@ -28,8 +28,14 @@ const DrawPallet = () => {
     { name: "yellow", color: "yellow" }
   ];
 
-  const handleBtn = () => {
-
+  const handleBtn = (btn) => { 
+    if(btn === "Undo") {
+      // need to setup undo button with canvas
+    } else if(btn === "Clear") {
+      // will probably have to setup a better way for state management
+    } else {
+      // setting up colors / brushes in future.
+    }
   }
 
   return (
@@ -38,20 +44,18 @@ const DrawPallet = () => {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="drawPallet__moveable--svg">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
-
       </div>
+
       <div className="drawPallet__stencils">
         {
           buttons.map((btn, i) =>
-            <button key={i} onClick={() => handleBtn()} className={btn.color ? "drawPallet--color" : "drawPallet--btn"} style={btn.color ? { backgroundColor: btn.color, height: '30px', width: '30px' } : {}}>
-              {btn.color ? "" : btn.name}
-            </button>
+            btn.color ?
+              <button key={i} onClick={() => handleBtn(btn.color)} className={btn.color ? "drawPallet--color" : "drawPallet--btn"} style={{ backgroundColor: btn.color, height: '30px', width: '30px' }}></button>
+              :
+              <button key={i} onClick={() => handleBtn(btn.name)} className="drawPallet--btn" >{btn.name}</button>
           )
         }
         <input type="color" id="colorPicker"></input>
-        {/* // <button onClick={handleBtn()} key={i} className={btn.color ? "drawPallet--color" : "drawPallet--btn"} style={btn.color ? { background: btn.color}: ""}>1
-        //   {btn.color ? "" : btn.name}
-        // </button> */}
       </div>
     </section>
   )
